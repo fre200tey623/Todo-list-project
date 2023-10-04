18ccc56e5e3b6f1389867a19ba55136b106ccb0e
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-10-04 19:22:45 -0300

    commit inicial

---
+++ back-end/ToDoList/Models/Atividades.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList.Models
+{
+    public class Atividades
+    {
+        public int AtividadesId { get; set; }
+        public string? Descricao { get; set; }
+        public DateTime HoraDeCriacao { get; set; }
+    }
+}
